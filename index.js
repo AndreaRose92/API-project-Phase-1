de331cec2313fd8071e9a01e35761193591fdbf0
Kelly Duran <kaduran16@gmail.com>
2022-07-12 10:49:13 -0400

    status not ok message added

--- index.js
+++ index.js
@@ -68,20 +68,27 @@ const myTeam = document.querySelector('.myTeam')
 
 const formPoke = document.querySelector("#pokeSearchForm")
 // console.log(formPoke, "you clicked me")
+const cardContainer = document.querySelector('.cardContainer')
 
 formPoke.addEventListener("submit", (event)=>{
     event.preventDefault()
     // console.log(event, "stop poking me!")
     const wantedPoke = event.target.searchBox.value
     fetch(`https://pokeapi.co/api/v2/pokemon/${wantedPoke}`)
-    .then(r => r.json())
-    .then(wantedPoke => generateSearchPokemonCard(wantedPoke))
-    console.log(wantedPoke)
+        .then(r => { 
+            console.log(r)
+            if (r.ok) {
+                r.json().then(wantedPoke => generateSearchPokemonCard(wantedPoke))
+            } else {
+                cardContainer.innerText= "This species of Pokemon hasn't been discovered yet! Try again!"
+            }
+            
+        })
     formPoke.reset()
 } )
 
   function generateSearchPokemonCard (wantedPoke){
-    const cardContainer = document.querySelector('.cardContainer')
+    
     cardContainer.innerHTML = ""
     generatePokemonCard(wantedPoke)
-  }  
+  }
